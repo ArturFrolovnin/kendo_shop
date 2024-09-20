c7abdc6baa513abb096f035130f4a1d5f8ae351c
Artur Frolovnin <arturfrolovnin@gmail.com>
2024-09-20 21:47:33 +0300

    add configurations for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
+export default defineConfig({
+  plugins: [vue(), basicSsl()],
+  build: {
+    outDir: "./docs",
+  },
+  base: "./",
+});
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+// export default defineConfig({
+//   plugins: [vue()],
+// });
