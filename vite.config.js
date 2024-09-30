228aaa934e6127be966b253509dfa21fe089a60a
Artur Frolovnin <arturfrolovnin@gmail.com>
2024-09-30 23:53:54 +0300

    hot fix config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
+  base: "/kendo_shop/",
   plugins: [vue(), basicSsl()],
   build: {
     outDir: "./docs",
